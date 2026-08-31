103aa84ab0eda86d1c11924d384b14bbcfa3f424
Pankaj <pc620042@gmail.com>
2026-08-31 18:24:01 +0530

    fixed the issues in the authentication process and add rate limiter for the api calls

--- client/app/api/auth/auth.ts
+++ client/app/api/auth/auth.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 export const registerSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
   email: z.email("Invalid email address"),
-  role: z.enum(["CUSTOMER", "WORKER"]),
+  role: z.enum(["Customer", "Worker","Contractor"]),
   password: z.string().min(8, "Password must be at least 8 characters"),
   phone: z.string().optional()
 });
